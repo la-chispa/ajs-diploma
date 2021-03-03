0db87d7280c1947cf0502403f7f29a448fe9e2e6
Olga Belotserkovskaya <olla.chispa@gmail.com>
2021-03-03 14:00:31 +0600

    add: two-dimensional board

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -10,6 +10,8 @@ export default class GameController {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi(themes.prairie);
+    this.setTwoDimensionalBoard();
+    console.log(this);
   }
 
   onCellClick(index) {
@@ -22,5 +24,17 @@ export default class GameController {
 
   onCellLeave(index) {
     // TODO: react to mouse leave
+  }
+
+  setTwoDimensionalBoard() {
+    this.gamePlay.coords = [];
+    for (let y = 0; y < this.gamePlay.boardSize; y += 1) {
+      for (let x = 0; x < this.gamePlay.boardSize; x += 1) {
+        const cell = {};
+        cell.x = x;
+        cell.y = y;
+        this.gamePlay.coords.push(cell);
+      }
+    }
   }
 }
